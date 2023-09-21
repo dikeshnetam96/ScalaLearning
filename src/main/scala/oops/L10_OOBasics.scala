22ee7598ebb2abff33c897ae3562a473b657128f
Dikesh Kumar <dikeshnetam96@gmail.com>
2023-09-22 00:44:19 +0530

    Adding Project

---
+++ src/main/scala/oops/L10_OOBasics.scala
@@ -0,0 +1,27 @@
+package oops
+
+object OOBasics extends App{
+val person = new Person("john",26)
+println(person)
+  // print(person.name)
+  println(person.age)
+  //person.greet("daniel")
+  person.greet()
+}
+
+//class Person and constructor on the same time
+class Person(name:String,val age:Int) { // name and age is paramenters not class members means you can not call or access it by person.name or age
+  // to do so we need to write val in front of paramenter variable so the it can become class member.
+  val x =2
+  println(1+3)
+
+  // function overloading
+  def greet(name:String) : Unit = println(s"${this.name} says : Hi, $name")
+  def greet() :Unit = println(s"hi name is ${name}")
+
+  // multiple constructors
+
+  def this(name:String) = this(name,0)
+  def this()=this("temp name")
+
+}
